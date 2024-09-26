2deac8e9cd4eb0f53d6ca60c9da27bfa7ba982b9
Gavin1937 <gyh1837771475gyh@gmail.com>
2024-09-26 22:12:52 +0800

    add invalid id check & param profile

--- 02.ChangeProfile.ps1
+++ 02.ChangeProfile.ps1
@@ -1,3 +1,7 @@
+param (
+    [Parameter(Mandatory=$false)][string]$profileName = ""
+)
+
 $KBMGR_DEFAULT_CONF = "$env:LOCALAPPDATA\Microsoft\PowerToys\Keyboard Manager\default.json"
 $PROFILE_LOCATION = "$PSScriptRoot\profiles"
 
@@ -8,16 +12,48 @@ if ((Test-Path $PROFILE_LOCATION) -eq $false)
 }
 
 
-# prompt for profile
-$default_profile = (Get-ChildItem default_backup | Sort-Object CreationTime | Select-Object -Last 1)
+$profile_idx = -1
+$profileName = $profileName.Trim().ToLower()
 $profiles = (Get-ChildItem $PROFILE_LOCATION)
-For ($i=1; $i -le $profiles.Count; $i++)
+
+# parameter profile
+if ($profileName.Length -gt 0)
 {
-    $profilename = ($profiles[$i-1] | Select-Object BaseName).BaseName
-    Write-Output "$i) $profilename"
+    if ($profileName -eq "default" -or $profileName -eq "0")
+    {
+        $profile_idx = 0
+    }
+    else
+    {
+        for ($i=1; $i -le $profiles.Count; $i++)
+        {
+            $locProfilename = ($profiles[$i-1] | Select-Object BaseName).BaseName.ToLower()
+            if ($locProfilename -eq $profileName)
+            {
+                $profile_idx = $i
+                break
+            }
+        }
+    }
 }
-Write-Output "0) default profile"
-$profile_idx = $(Read-Host -Prompt "Enter profile index")
+# prompt for profile
+else
+{
+    for ($i=1; $i -le $profiles.Count; $i++)
+    {
+        $locProfilename = ($profiles[$i-1] | Select-Object BaseName).BaseName
+        Write-Output "$i) $locProfilename"
+    }
+    Write-Output "0) default profile"
+    try
+    {
+        $profile_idx = [int]$(Read-Host -Prompt "Enter profile index")
+    }
+    catch
+    {
+        $profile_idx = -1
+    }
+}
 
 
 # change profile
@@ -25,6 +61,11 @@ if ($profile_idx -eq 0)
 {
     $selected_profile_basename = "default"
     .\RestoreDefault.ps1
+}
+elseif (1 -isnot [int] -or $profile_idx -lt 0 -or $profile_idx -gt $profiles.Count)
+{
+    Write-Output "Invalid profile input"
+    exit -1
 }
 else
 {
